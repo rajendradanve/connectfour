7437e2b6267335467732e75724c0f02e834d0586
Rajendra <rajendradanve@gmail.com>
2021-04-23 21:34:05 +0000

    basic game container layout create with index.html and css file. my-script.js file update to calculate width to determine main game area

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -1,1 +1,9 @@
-alert("test");
+let gameContainerWidth = $("#game-controller").css("width");
+let gameContainerHeight = $("#game-controller").css("height");
+
+let gameAreaWidth = parseFloat(gameContainerWidth.slice(0,-2))*0.6;
+let gameAreaHeight = parseFloat(gameContainerHeight.slice(0,-2))*0.9;
+
+console.log(gameAreaHeight);
+console.log(gameAreaWidth);
+
