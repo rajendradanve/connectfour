139bf63503f2945207abf4eb3cb519dd8f662d54
Rajendra <rajendradanve@gmail.com>
2021-04-29 15:24:52 +0000

    animation of pokeball working. Code to be improve or simplify if possible

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -5,16 +5,17 @@ let windowHeight = window.innerHeight;
 
 $(".random-coin").css("width", randomCoinSize);
 $(".random-coin").css("height", randomCoinSize);
-$(".random-coin").css("top", -randomCoinSize);
-
-setInterval(coinBackGroundAnimation, 5000);
-
-
+$(".random-coin").css("top", -randomCoinSize*2);
+setInterval(coinBackGroundAnimation, 1000);
 })
 
 function coinBackGroundAnimation(){
 
-    $(".random-coin").animate({top: `${window.innerHeight}`}, 5000).fadeOut().animate({top: `-100`}).fadeIn();
+    for(let i=1; i<=5;i++){
+        $(`#random-coin-${i}`).animate({top: `${window.innerHeight}`}, (Math.random()*5000+2000)).fadeOut().animate({top: `-100`}).fadeIn();
+    }
+
+    //$(".random-coin").animate({top: `${window.innerHeight}`}, 5000).fadeOut().animate({top: `-100`}).fadeIn();
 
    /* let endBottomPosition = windowHeight + randomCoinSize ;
 
