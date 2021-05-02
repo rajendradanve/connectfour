2664fb9d54537e4fe2ba5eca5df311a7be12d28f
Rajendra <rajendradanve@gmail.com>
2021-05-02 21:26:26 +0000

    added player selection and disabling computer selection

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -7,6 +7,26 @@ $(".random-coin").css("width", randomCoinSize);
 $(".random-coin").css("height", randomCoinSize);
 $(".random-coin").css("top", -randomCoinSize*2);
 
+$("input").click(function(){
+
+    if($(this).val()==="player-1-computer"){
+
+        $("#player-2-computer").attr('disabled', true);
+        $("#player-2-computer").attr('checked', false);
+
+    }else if($(this).val()==="player-2-computer"){
+        
+        $("#player-1-computer").attr('disabled', true);
+        $("#player-1-computer").attr('checked', false);
+        
+    }else if($(this).val()==="player-1-human" || $(this).val()==="player-2-human" ) {
+        $("#player-1-computer").attr('disabled', false);
+         $("#player-2-computer").attr('disabled', false);
+    }
+
+});
+
+
 })
 
 function redirectToGamePage(){
