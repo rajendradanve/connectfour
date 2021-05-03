b6d56c98d8c9d5a95c9166b49e4ae2c0eea37b1d
Rajendra <rajendradanve@gmail.com>
2021-05-03 13:00:42 +0000

    started working on redirectToGamePage function

--- assets/js/main-page.js
+++ assets/js/main-page.js
@@ -1,53 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-let randomCoinSize = window.innerWidth * 0.1;
-let windowHeight = window.innerHeight;
+    let randomCoinSize = window.innerWidth * 0.1;
+    let windowHeight = window.innerHeight;
 
-$(".random-coin").css("width", randomCoinSize);
-$(".random-coin").css("height", randomCoinSize);
-$(".random-coin").css("top", -randomCoinSize*2);
+    $(".random-coin").css("width", randomCoinSize);
+    $(".random-coin").css("height", randomCoinSize);
+    $(".random-coin").css("top", -randomCoinSize * 2);
 
-$("input").click(function(){
-    setPlayer(this);
+    $("input").click(function () {
+        setPlayer(this);
 
-});
+    });
 
 })
 
-function setPlayer(player){
+//function for setting up player option. 
+// Code made so that both players can not be computer.
+function setPlayer(player) {
 
-     if($(player).val()==="player-1-computer"){
-            
+    if ($(player).val() === "player-1-computer") {
+
         $("#player-2-computer").attr('disabled', true);
         $(".player-2-computer-label").css('opacity', '.2');
         $("#player-2-human").attr('checked', true);
 
-    }else if($(player).val()==="player-2-computer"){
-        
+    } else if ($(player).val() === "player-2-computer") {
+
         $("#player-1-computer").attr('disabled', true);
         $(".player-1-computer-label").css('opacity', '.2');
         $("#player-1-human").attr('checked', true);
-        
-    }else {
-        
+
+    } else {
+
         $("#player-1-computer").attr('disabled', false);
         $(".player-1-computer-label").css('opacity', '1');
         $("#player-2-computer").attr('disabled', false);
         $(".player-2-computer-label").css('opacity', '1');
-         
+
     }
 
     return;
 
-      
+
 }
 
-function redirectToGamePage(){
+function redirectToGamePage() {
 
-   
+    //getting selected player values for radio button
+    let player1 = $('input[name="player-1"]:checked').val();
+    let player2 = $("input[name='player-2']:checked").val();
+
+    console.log(player1);
+    console.log(player2);
     
+
 }
-
-
-
-
