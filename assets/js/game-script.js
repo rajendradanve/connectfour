0cab3bf111b5827bc39e8ab6f9126714fec03706
Rajendra <rajendradanve@gmail.com>
2021-05-06 22:56:49 +0000

    game-script.js file updated and logic is working fine

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -1,10 +1,160 @@
+const player1Coin = "red-coin";
+const player2Coin = "yellow-coin";
+const clearCell = "empty-coin";
+
 $(document).ready(function() {
     setGameArea();
 
     let player2 = getPlayerInfo();
 
+    startGame(player2);
+
 })
 
+function startGame(player2){
+
+    let currentPlayer = 1;
+    
+
+    for (let i = 0; i < 7; i++) {
+
+         $(`#column${i}`).mouseenter(function() {
+
+            if (checkEmptyClass(i, 5)) { //only allowed if top last row in game board is not filled with coin.
+                
+                removeAddClass(i, 6, clearCell, activePlayerCoin(currentPlayer)); // not show coin in the top entry row as no empty cell available.
+            }
+        }).mouseleave(function() {
+
+            removeAddClass(i, 6, activePlayerCoin(currentPlayer));
+
+        }).click(function() {
+
+            let column = i;
+
+             if (checkEmptyClass(i, 5)) { //only allowed if last row (6th) in game board is not filled with any coin
+                    
+               let row = addActivePlayerCoin(i, activePlayerCoin(currentPlayer), clearCell); //add coin of active player in the selected column and available row.
+                
+               let isGameOn = checkIfWin(column, row, activePlayerCoin(currentPlayer));
+               
+               if(isGameOn === 0){
+                   //continue Game. change player change class
+                   currentPlayer= changePlayer(currentPlayer);
+
+                   if(currentPlayer === 2 && player2 === 'Computer'){
+                        let computerWinCheck = computerPlayer();
+                        if(computerWinCheck === 0){
+                        currentPlayer= changePlayer(currentPlayer);
+                        }else if(computerWinCheck === 1){
+                            //computer won
+                            console.log ("computer won");
+                        }else if(computerWinCheck === 2){
+                            //game is draw
+                            console.log("Game is drawn by computer");
+                        }
+                   }
+
+               }else if(isGameOn === 1){
+                   //current player is winner
+                   console.log(currentPlayer+ " own");
+
+               }else if(isGameOn === 2){
+                   //game is draw
+                   console.log("Game is draw");
+               }else{
+                    //something is wrong
+                    console.log("something is wrong");
+               }
+           
+            }
+        });
+    }
+}
+
+
+//function when one of the player is computer. Function will find random cell to put coin.
+function computerPlayer() {
+    let activePlayer = 2; //if computer is playing, It is always 2nd player as per current game.
+    let computerCoin = activePlayerCoin(activePlayer);
+
+    let isComputerWin = 0;
+    let isComputerPlayed = false;
+
+    while (!isComputerPlayed) {
+        let computerColumn = Math.floor(Math.random() * 7);
+
+        if (checkEmptyClass(computerColumn, 5)) {
+
+            let computerRow = addActivePlayerCoin(computerColumn, computerCoin);
+            isComputerPlayed = true;
+            isComputerWin = checkIfWin(computerColumn, computerRow, computerCoin)
+            break;
+        }
+
+    }
+
+    return isComputerWin;
+
+}
+
+
+//function to add active player coin in the empty cell and remove column if place is not available. 
+function addActivePlayerCoin(selectedColumn, playerCoin) {
+    
+    let rowNumber = 0;
+    
+    while (rowNumber < 6) { // rowNumber required to checked only 6 rows from bottom.
+
+        if (checkEmptyClass(selectedColumn, rowNumber)) {
+
+            removeAddClass(selectedColumn, rowNumber, clearCell, playerCoin);
+
+            if (rowNumber === 5) { //if coin goes to top last row of the game board then that column shall not be used for further play.
+                
+                $(`#coin${selectedColumn}6`).removeClass(player1Coin).removeClass(player2Coin).addClass(clearCell);
+            }
+
+            break; //come out of for loop for rowNumber after condition is satisfied once.
+        }
+
+        rowNumber++;
+    }
+
+    //removing coinClass for current player so that another player can play.
+    removeAddClass(selectedColumn, 6, playerCoin, "empty-coin");
+    return rowNumber;
+}
+
+//function to check if cell is empty to add coin
+function checkEmptyClass(column, row) {
+
+    return $(`#coin${column}${row}`).hasClass("empty-coin")
+
+}
+
+//function to remove class and add another class from cell
+function removeAddClass(column, row, removeClass, addClass) {
+
+    return $(`#coin${column}${row}`).removeClass(removeClass).addClass(addClass);
+}
+
+//setup active player coin
+
+function activePlayerCoin(playerNumber){
+
+    let activeCoin = playerNumber ===1? player1Coin : player2Coin;
+
+    return activeCoin;
+}
+
+// change player
+function changePlayer(currentPlayer){
+    
+    let newPlayer = currentPlayer === 1? 2 : 1;
+
+    return newPlayer;
+}
 
 function getPlayerInfo() {
 
@@ -72,4 +222,281 @@ function setGameArea() {
 
 
     return;
+}
+
+
+
+
+
+/* checking if any same color 4 coins are in a row or diagonal. 
+function takes current row and column where coin goes last, active coin class.
+This function checks if there are any four same coins in a
+    1. column - downward
+    2. row - right side or left side
+    3. Diagonal - left to right
+    4. diagonal - right to left
+
+    function will return true if any of above condition satified. 
+*/
+
+//checking if any of winning condition satisfied
+function checkIfWin(i, j, winningCheckClass) {
+
+    let winningInt = 0;
+
+    if (isColumnWinning(i, j, winningCheckClass) || isRowWining(i, j, winningCheckClass) || isDiagonalWinning(i, j, winningCheckClass)) {
+
+        winningInt = 1; // winning condition satisfied. stop game.
+
+    } else if (isGameDraw()) {
+
+        winningInt = 2; // game is draw. No empty cells to add coins. No one wins
+
+    } else {
+
+        winningInt = 0;
+    }
+
+    return winningInt;
+
+}
+
+function isColumnWinning(column, row, coinColorClass) {
+
+    let numberOfCoinsColumn = 1; //to check if 4 coins are in a row. 
+    let isColumnWinner = false;
+    if (row > 2) { //don't need to check column winning condition if row is less than 3. 
+
+
+        //for loop to check if there are same coins in a column
+        for (let j = (row - 1); j >= (row - 3); j--) {
+
+            if ($(`#coin${column}${j}`).hasClass(coinColorClass)) {
+                numberOfCoinsColumn++;
+            }
+
+            if (numberOfCoinsColumn == 4) {
+                isColumnWinner = true;
+            }
+        }
+
+    }
+    return isColumnWinner;
+}
+
+//Below function check if four same coins are in a same row.
+function isRowWining(column, row, coinColorClass) {
+
+    let numberOfCoinsRow = 1;
+    let isRowWinner = false;
+    let minColumnCheck = column <= 3 ? 0 : (column - 3); //only need to check max 3 columns on left if available
+    let maxColumnCheck = column >= 3 ? 6 : (column + 3); // only need to check max 3 column on right if available
+    let i = column - 1;
+
+    //below while loop checking left side of the row if same conis are present
+
+    while (i >= minColumnCheck) {
+
+        if ($(`#coin${i}${row}`).hasClass(coinColorClass)) {
+
+            numberOfCoinsRow++;
+
+        } else {
+            break; // loop breaks if immidate cells don't have same class. 
+        }
+
+        i--;
+    }
+
+    //below while loop checks right of the row if same class present
+
+    if (numberOfCoinsRow != 4) {
+
+        let j = column + 1;
+
+        while (j <= maxColumnCheck) {
+
+            if ($(`#coin${j}${row}`).hasClass(coinColorClass)) {
+
+                numberOfCoinsRow++;
+
+
+            } else {
+                break; // loop breaks if immidate cells don't have same class. 
+            }
+
+            j++;
+
+        }
+
+    }
+
+    if (numberOfCoinsRow == 4) {
+        isRowWinner = true;
+    }
+
+
+
+    return isRowWinner;
+
+
+}
+// Below function check if four coins are same diagonally.
+function isDiagonalWinning(column, row, coinColorClass) {
+
+    let diagonalWinner = false;
+
+
+    //left to right check. As per testing condition for left top cells and right bottom cells diagonal winning is not possible.
+    // below if statement is used to remove above cells from checking.
+
+    if ((row > column && (row - column) >= 3) || (row < column && (column - row) >= 4)) {
+
+        diagonalWinner = false;
+
+    } else {
+
+        let numberOfDiagonalLeftToRight = 1; // for left to right condition. Value one is given as current cell will already pass the test
+        let minColumnCheckLeftToRight = column <= 3 ? 0 : (column - 3); //only need to check max 3 columns on left if available
+        let maxColumnCheckLeftToRight = column >= 3 ? 6 : (column + 3); // only need to check max 3 column on right if available
+        let minRowCheckLeftToRight = row <= 3 ? 0 : (row - 3); // only need to check 3 down rows if available
+        let maxRowCheckLeftToRight = row >= 3 ? 5 : (row + 3); // only need to check 3 up rows if available of max 5
+        let i = column - 1; //to check downward items. 
+        let j = row - 1; // to check downward items
+
+        //below while loop check diagonal cells to left side (if cells are available)
+        while (i >= minColumnCheckLeftToRight && j >= minRowCheckLeftToRight) {
+
+            if ($(`#coin${i}${j}`).hasClass(coinColorClass)) {
+
+                numberOfDiagonalLeftToRight++;
+
+            } else {
+                break; // loop breaks if immidate cells don't have same class. 
+            }
+
+            i--;
+            j--;
+        }
+
+        //if condition check winning condition already acheived or not
+        if (numberOfDiagonalLeftToRight != 4) {
+
+            //reassign value to i and j
+            i = column + 1;
+            j = row + 1;
+            //Below while loop check diagonal cell to right (if cells are available)
+            while (i <= maxColumnCheckLeftToRight && j <= maxRowCheckLeftToRight) {
+
+                if ($(`#coin${i}${j}`).hasClass(coinColorClass)) {
+
+                    numberOfDiagonalLeftToRight++;
+
+                } else {
+                    break; // loop breaks if immidate cells don't have same class. 
+                }
+                i++;
+                j++;
+
+            }
+
+        }
+
+        if (numberOfDiagonalLeftToRight == 4) {
+            diagonalWinner = true;
+        }
+
+    }
+
+    //Right to Left check
+    // Right to left check required to done if left to right condition doesn't satisfy winning condition.
+    if (!diagonalWinner) {
+
+        // As per testing condition left bottom cells and right top cells diagonal winning is not possible.
+        // below if statement is used to remove above cells from checking.
+        if ((row + column) <= 2 || (column + row) >= 9) {
+
+            diagonalWinner = false;
+
+        } else {
+
+            let numberOfDiagonalRightToLeft = 1; // for Rith to left condition. Value one is given as current cell will already pass the test
+
+            let minColumnCheckRightToLeft = column <= 3 ? 0 : (column - 3); //only need to check max 3 columns on left if available
+            let maxColumnCheckRightToLeft = column >= 3 ? 6 : (column + 3); // only need to check max 3 column on right if available
+            let minRowCheckRightToLeft = row <= 3 ? 0 : (row - 3); // only need to check 3 down rows if available
+            let maxRowCheckRightToLeft = row >= 3 ? 5 : (row + 3); // only need to check 3 up rows if available of max 5
+
+            //reassigning values to i (column) and j (rows)
+            i = column + 1; //to check downward items towards right . Number of column increases towards right
+            j = row - 1; // to check downward items towards right
+
+            //below while loop check diagonal cells to left side (if cells are available)
+            while (i <= maxColumnCheckRightToLeft && j >= minRowCheckRightToLeft) {
+
+                if ($(`#coin${i}${j}`).hasClass(coinColorClass)) {
+
+                    numberOfDiagonalRightToLeft++;
+
+                } else {
+                    break; // loop breaks if immidate cells don't have same class. 
+                }
+
+                i++;
+                j--;
+            }
+
+            //if condition check winning condition already acheived or not
+            if (numberOfDiagonalRightToLeft != 4) {
+
+                //reassign value to i and j
+                i = column - 1;
+                j = row + 1;
+                //Below while loop check diagonal cell to right (if cells are available)
+                while (i >= minColumnCheckRightToLeft && j <= maxRowCheckRightToLeft) {
+
+                    if ($(`#coin${i}${j}`).hasClass(coinColorClass)) {
+
+                        numberOfDiagonalRightToLeft++;
+
+                    } else {
+                        break; // loop breaks if immidate cells don't have same class. 
+                    }
+                    i--;
+                    j++;
+
+                }
+
+
+            }
+
+            if (numberOfDiagonalRightToLeft == 4) {
+                diagonalWinner = true;
+            }
+
+        }
+    }
+
+    return diagonalWinner;
+}
+
+function isGameDraw() {
+
+    let gameDrawStatus = true;
+
+    for (let i = 0; i < 7; i++) {
+
+        if (checkEmptyClass(i, 5)) {
+            gameDrawStatus = false;
+            break;
+        }
+
+    }
+    return gameDrawStatus;
+}
+
+function gameFinish(winnerPlayer) {
+
+    console.log("winner player is " + winnerPlayer);
+
 }
